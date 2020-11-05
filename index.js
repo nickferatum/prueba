5ca7ae4b4591216057872c790448f1e944fb55be
nickferatum <nicolasrey01@hotmail.com>
2020-11-04 19:47:18 -0500

    cargue 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (solicitud, respuesta) => {
+    respuesta.send('<h1>hola</h1>');
+})
+
+app.listen(3000, ()=>{
+    console.log('el servidor ya esta encendido http://localhost:3000');
+})
